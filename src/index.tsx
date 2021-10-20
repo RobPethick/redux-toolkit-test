07fbe19f31dadc6b9ae54333c0165f7bda3ee214
Rob Pethick <rob@beedle.co>
2021-10-20 09:42:11 +0100

    Theming

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,27 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import AvailabilityData from './data/availability-mock.json';
 import CounsellorData from './data/counsellor-mock.json';
 import { importAvailabilities, importCounsellors } from './features/bookingGrid/bookingGridSlice';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 store.dispatch(importCounsellors(CounsellorData));
 store.dispatch(importAvailabilities(AvailabilityData));
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#35D0BA',
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <LocalizationProvider dateAdapter={DateAdapter}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </LocalizationProvider>
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </LocalizationProvider>
+    </ThemeProvider >
   </React.StrictMode>,
   document.getElementById('root')
 );
